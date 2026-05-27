cca5abe49553e211b647642f6e6ff7c0964832cc
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-05-27 14:19:54 +0200

    exo5

---
+++ ex5/ft_data_stream.py
@@ -0,0 +1,18 @@
+from typing import Generator
+import random
+
+def gen_event() -> Generator[tuple[str, str], None, None]:
+    names = ["bob", "alice", "dylan", "charlie"]
+    actions = ["run", "eat", "sleep", "grab", "move", "climb", "swim", "use", "release"]
+    while True:
+        yield 
+    
+
+
+def main()
+    print("=== Inventory System Analysis ===")
+
+
+
+if __name__ == "__main__"
+    main()
